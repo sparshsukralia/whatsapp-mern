efb8b82d050ddb4a98e39cc5af6a15b2ae02fee8
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-10-27 21:39:57 +0545

    chat header

--- whatsapp-frontend/src/Chat/Chat.js
+++ whatsapp-frontend/src/Chat/Chat.js
@@ -1,8 +1,33 @@
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import React from "react";
 import "./Chat.css";
 
 const Chat = () => {
-  return <div className="chat">{/* <h1>Chat Box</h1> */}</div>;
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at ...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
