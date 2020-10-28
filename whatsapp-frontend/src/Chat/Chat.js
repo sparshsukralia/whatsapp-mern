ef000006a22094df957631b0fe1e99a5d2da6dab
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-10-28 22:40:11 +0545

    chat messages structure

--- whatsapp-frontend/src/Chat/Chat.js
+++ whatsapp-frontend/src/Chat/Chat.js
@@ -32,6 +32,18 @@ const Chat = () => {
           This is a message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
+
+        <p className="chat__receiver">
+          <span className="chat__name">Sparsh</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Sparsh</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
       </div>
     </div>
   );
