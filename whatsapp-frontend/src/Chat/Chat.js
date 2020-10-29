489b58ed4532da98998b5d2288cf8b41bd29aa03
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-10-29 19:33:56 +0545

    chat footer input

--- whatsapp-frontend/src/Chat/Chat.js
+++ whatsapp-frontend/src/Chat/Chat.js
@@ -1,6 +1,7 @@
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 import React from "react";
 import "./Chat.css";
 
@@ -49,6 +50,11 @@ const Chat = () => {
 
       <div className="chat__footer">
         <InsertEmoticonIcon />
+        <form>
+          <input placeholder="Type a message..." type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
